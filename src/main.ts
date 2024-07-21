8163590185e04a67b32d1941d3e0cae5bc19489b
Shakar Kosar <shakarkosar01@gmail.com>
2024-07-21 10:21:28 +0300

    ref: removed unnecassary response interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CacheControlInterceptor } from '@/common/interceptors/cache-control.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -9,7 +8,6 @@ async function bootstrap() {
 		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
 		allowedHeaders: 'Content-Type, Accept, Cache-Control, Pragma, Expires'
 	});
-	app.useGlobalInterceptors(new CacheControlInterceptor());
 	await app.listen(3000);
 }
 bootstrap().finally();
